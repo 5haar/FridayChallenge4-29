3e4b9051cc30b4933813452dc4adb6dcd1fc7550
Shaar <shaar@MacBook-Pro-2.local>
2022-04-29 14:49:18 -0400

    Second commit: some more changes

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ arrowAdd100 = (num1) => num1 + 100;
 
 console.log(arrowAdd100(200));
 
-// 3. (This is my attempt but I could not for some reason figure it out. I found other ways of doing it on Google but )
+// 3. (This is my attempt but I could not for some reason figure it out.)
 
 countLetters = (statement, letter) => {
   for (let i = 0; i <= statement.length; i++) {
@@ -27,15 +27,17 @@ countLetters = (statement, letter) => {
 
 console.log(countLetters("hello", "l"));
 
-countLetters2 = (str) => {
-  let letters = 0;
-  let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let arr = alphabet.split("");
-  for (let i = 0; i < str.length; i++) {
-    if (arr.indexOf(str[i]) > -1) {
-      letters = letters + 1;
-    }
-  }
-  return letters;
-};
-console.log(countLetters2("Hello World"));
+// Another attempt
+
+// countLetters2 = (str) => {
+//   let letters = 0;
+//   let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   let arr = alphabet.split("");
+//   for (let i = 0; i < str.length; i++) {
+//     if (arr.indexOf(str[i]) > -1) {
+//       letters = letters + 1;
+//     }
+//   }
+//   return letters;
+// };
+// console.log(countLetters2("Hello World"));
